3c98ce9e0b94aa3fa6837087c02c3f1fa7df91cb
James Traver <jtraverjr@gmail.com>
2016-03-08 11:04:24 -0600

    .animate()

--- scripts/animations.js
+++ scripts/animations.js
@@ -5,16 +5,51 @@ $(document).ready(function(evt) {
   // create some events to hide '.news' and '.weather'
   $('#weather-btn').click(function(evt) {
     // select '.weather' and .hide() it
-    $('.weather').hide();
+    //$('.weather').hide();
+    $('.weather').slideUp();
   });
   // now hide '.news' and .hide() it
   $('#news-btn').click(function(evt) {
-    $('.news').hide();
+    //$('.news').hide();
+    $('.news').slideUp();
   });
   // what if we want to see them all again?
   $('#show-and-hide-btn').click(function(evt) {
     // we miss you! .show() to come back
-    $('.news').show();
-    $('.weather').show();
+    // $('.news').show();
+    // $('.weather').show();
+    $('.news').slideDown();
+    $('.weather').slideDown();
+  });
+  // animation station
+  $('#move-img').click(function(evt) {
+    // .animate() - argument {}
+    // .animate(propertiesObject, millisecondsToTake, callback)
+    // $('img').animate({
+    //   opacity: 0.8,
+    //   width: '25%',
+    //   borderWidth: '5px'
+    // }, 1000, function() {
+    //   console.log('Simba has been moved');
+    // });
+    $('img').animate({
+      opacity: 0.8,
+      width: '25%',
+      borderWidth: '5px',
+      top: '+=500px',
+      left: '+=250px'
+    }, 1000, function() {
+      console.log('Simba has been moved');
+      // then animate again!
+      $('img').animate({
+        opacity: 0.8,
+        width: '25%',
+        borderWidth: '5px',
+        top: '-=500px',
+        right: '-=900px'
+      }, 500, function(evt) {
+        console.log('Simba moved again!')
+      })
+    });
   });
 });
